26a27e763507d12abf7e13cc9b492c8caef7e790
vcdgrass <duylk25@gmail.com>
2025-12-01 10:15:38 +0700

    reset

--- frontend/src/api/coursesApi.js
+++ frontend/src/api/coursesApi.js
@@ -10,6 +10,13 @@ const coursesApi = {
     getById: (id) => {
         return axiosClient.get(`/courses/${id}`);
     },
+    createSection: (courseId, data) => {
+        return axiosClient.post(`/courses/${courseId}/sections`, data);
+    },
+    createModule: (sectionId, data) => {
+        // data: { title, url, type: 'resource_url' }
+        return axiosClient.post(`/courses/sections/${sectionId}/modules`, data);
+    },
 };
 
 export default coursesApi;
